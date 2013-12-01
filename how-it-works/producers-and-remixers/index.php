1f7e619f678550125cfb0fa7521177d70071ea9f
jonskinner <jonskinner@jonpersonal.home>
2013-12-01 15:56:02 +0000

    content all updated

--- how-it-works/producers-and-remixers/index.php
+++ how-it-works/producers-and-remixers/index.php
@@ -6,7 +6,7 @@
 <div class="container-fluid">
 	<section class="row-fluid">
 		<div class="span12">
-			<h1 class="topHeading">Producers &amp; Remixers</h1>
+			<h1 class="topHeading">Record Producers &amp; Remixers</h1>
 		</div>
 	</section>
 </div>
@@ -40,63 +40,129 @@
 						<img src="/images/how-it-works/producers/djrap.png" alt="" />
 					</li>
 				</ul>
-				<h2>Production, Remixing and Studio Work</h2>
 				<p>
-					You’re a producer, you want to earn money, get hired and match your music with client needs.
-					Well that is exactly what we do! We stream line the way you make connections and receive work 
-					from singers, artists &amp; bands around the world, together with publishers and record labels 
-					looking to sign high quality production music.
+					Most producers are multi functional and talented individuals, you may class yourself strictly as a record producer, 
+					but you may well write songs and offer other specialist services including artist development and management of 
+					new artists. Your role is to bring to life a song and best meet the client's needs.
 				</p>
-				<h3>Remixers &amp; Remix work</h3>
-				<p>
-					We have a wealth of remix work provided from a host of established Record Labels around the 
-					world and direct from independent artists, some of these include xxxxxxxxxx
-				</p>
-				<h3>Collaborate &amp; Co-write</h3>
-				<p>
-					As Producers, using our Collaboration Project type allows you to co-write and work with 
-					likeminded professionals on agreed terms.
-				</p>
-				<h3>How can I utilize my existing music &amp; productions?</h3>
-				<p>
-					If you own the master rights you can use our Collaboration Project type to match your music 
-					with other users needs. We also have a number of Publishers regularly looking for music to sign, 
-					so boot up those hard drives, as you can monetize any existing works through Music Gateway.
-				</p>
-				<h3>How does it work?</h3>
-				<ul class="funkyBullet">
-					<li>Once registered for free you set up your industry profile</li>
-					<li>Users post projects, defining their project need</li>
-					<li>We notify you of relevant project work</li>
-					<li>You review and PITCH (tender) for the work</li>
-				</ul>
-				<p>
-					Only if you secure the work will we charge an 11% commission on 
-					fixed free work or a small connection fee if you are collaborating.
+				<p>	
+					Music Gateway allows you to receive work opportunities, further your career, develop your skills and 
+					provide your services to clients from around the world. We give you the vehicle to place your music with 
+					likeminded professionals, commission work from session musicians and manage any personal projects through 
+					our unique platform.
 				</p>
 				<p>
-					Music Gateway is 100% free until the point you receive paid project work or a beneficial 
-					connection when collaborating. There are no hidden charges, posting projects are free* 
-					and we never take any form of royalty or publishing percentage whatsoever.
+					We aren't a traditional network, as we introduce clients in a targeted timesaving way, which stream lines the 
+					whole process. You can collaborate, receive work and connect with other industry roles, such as singers, 
+					songwriters, artists, bands, record labels, publishers, management and musicians on a global scale.  
 				</p>
-				<p class="innerTerm">* All projects types are free to post except for our ‘On Spec’ project type</p>
-				<h3>Transactions &amp; Getting paid</h3>
+				<h3>This is how it works</h3>
 				<p>
-					You manage and control transactions within a currency account. This allows you to receive, 
-					deposit and withdraw funds, trading within our global market place. All payments are securely 
-					held within legally protected client accounts in three denominations, US Dollars, UK pounds and Euros.
+					Simply <a href="/sign-up" title="Register" class="">register</a> and create your industry profile. This acts as your music portfolio and displayed to other users. 
+					It outlines your experience, skills, services, genres, video, audio and images. You can only get connected 
+					and receive work through projects posted on Music Gateway.
 				</p>
-				<h3>Protection &amp; Piece of mind</h3>
 				<p>
-					When receiving fixed fee work, you can request advance Interim payments, which are like escrow style holding 
-					payments. We use the very best SSL 2048 bit sever protection and security for your files and account access. 
-					Payments are processed through PacNet a global market leader is e-commerce and PayPal.
+					When a project is published, we match the details to your profile and send you a notification making 
+					the opportunity targeted and relevant.
 				</p>
-				<h3>File transfers &amp; Remote working</h3>
 				<p>
-					Each project is provided with a secure workspace area which allows you and your project workers to manage 
-					project files. Your workers can access and download stems and submit back their work for your review. 
-					We provide communication tools and timeline feedback on audio files. Check out FEATURES page for full details.
+					You are then able to review, pitch (also known as tending or quoting) for work and communicate privately 
+					with the client to discuss the details or negotiate any terms. Project owners can pre-qualify workers by 
+					reviewing their profile and listening to their music.
+				</p>
+				<p>
+					Music Gateway provides opportunity from both company and individual clients. You are also able to manage 
+					your own projects and reach out to other professionals on a global scale.
+				</p>  
+				<h3>How do projects work?</h3>
+				<p>
+					Projects are how you get connected to other professionals around the world, by defining a need or providing 
+					an opportunity, we match the details to user profiles, streamlining the whole process.
+				</p>
+				<p>
+					You can easily pre-qualify users pitching using our management tools, together with private messaging to 
+					discuss and negotiate any terms.
+				</p>
+				<p>
+					Each project is provided a unique and secure workspace, allowing for remote working features, global 
+					communication tools, file transfers and management of worker submissions.
+				</p>
+				<p>
+					Music Gateway is an all-in-one solution for any music project.
+				</p>
+				<h3>What is pitching?</h3>
+				<p>
+					Based on your profile, we sent your notifications of any relevant project work posted. You can easily 
+					review the project details and then pitch (also known as tendering or quoting) for the work. 
+					Depending on the project type, your pitch may include the fixed cost to supply your services or may 
+					be a request to collaborate and co-write with the user.
+				</p>
+				<p>
+					You can include private messages and discuss, negotiate any terms for each project. If your pitch is 
+					successful you are notified and can still chose to accept or reject their acceptance. Only at this 
+					juncture is either a commission or connection fee payable.*
+				</p> 
+				<h3>Are there any fees or costs?</h3>
+				<p>
+					All projects are free to post, excluding our premium On Spec project type. Charges are only payable 
+					if a user receives paid work or a connection.  
+				</p>
+				<p>
+					The level of fees depends on the type of account you choose and there is a free account option available. 
+					Note that regardless of the account type, we never take any form of royalty or publishing percentage 
+					% whatsoever.
+				</p>
+				<p>
+					Click here to see the account options and more information (PHIL can this be a GREEN BUTTON here)
+					<a href="/sign-up" title="Account Type" class="greenButton">Account Type</a>
+				</p>
+				
+				<p>PHIL these points below I believe should be links that expand down the information, so more like an FAQ to save space</p>
+				<h3>Key benefits for producers and remixers</h3>
+				<p>
+					<strong>Receive production and remix work</strong><br />
+					We receive a wealth of regular remix work that is provided from a 
+					host of established international Record Labels around the world and also directly from independent artists.  
+				</p>
+				<p>
+					<strong>Collaboration and song writing</strong><br />
+					Use our 'Collaboration' project type to connect and co-write 
+					with vocalists, rappers and singers. Work with like-minded musicians, songwriters and seek top lines 
+					to complete your projects, whilst staying in control of the terms.
+				</p>
+				<p>
+					<strong>Can I utilise my existing music, productions and songs?</strong><br />
+					Yes in many ways. If you own the master rights you can use our 'Collaboration' project type to match any 
+					available music with other users, including artists and vocalists for co-writes. We have regular Publishers 
+					looking to license music, so boot up those hard drives, as you can monetise any existing works through Music Gateway.
+				</p>
+				<p>
+					<strong>Transactions and getting paid</strong><br />
+					You manage and control transactions within your currency account. This allows you to receive, deposit 
+					and withdraw funds, trading within our global market place. All payments are securely held within legally 
+					protected client accounts in three denominations. US Dollars, UK pounds and Euros. There is a small charge for 
+					processing card payments which equal to the amount we are charged by our payment provider and PayPal.
+				</p>
+				<p>
+					<strong>Your protection and peace of mind</strong><br />
+					You can request advanced Interim payments when receiving fixed fee work, which are like escrow style holding 
+					payments offering security for both work and project owner. Our site is protected by an SSL certification for 
+					security and peace of mind knowing that your copyrighted files and account access is protected with the same 
+					security used by banks and other financial organisations.
+				</p>
+				<p>
+					<strong>File transfers and remote working</strong><br />
+					Each project is provided with a secure workspace area, which allows you and your project workers to manage 
+					project files. Your workers can access and download stems and submit back their work for your review. We provide 
+					communication tools and timeline feedback on audio files. Check out our <a href="/features" title="Features" class="">features</a>
+					for more information.
+				</p>
+				<p>
+					<strong>Pro level subscription</strong><br />
+					We offer two tiers of pro level subscription, which provides increased ranking and positioning throughout 
+					the site, a raised Pro Level profile, reduced commission and connection fees and increased or unlimited data 
+					storage depending on your plan. For more information <a href="/pro-level" title="Pro Level" class="">click here</a>
 				</p>
 				</div>
 		</div>
