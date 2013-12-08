715619d83c2420bef0bfbfb622512002d584afa7
philcarr88 <phil.carr@musicgateway.net>
2013-12-08 22:57:01 +0000

    Updates to features page

--- features/index.php
+++ features/index.php
@@ -104,6 +104,129 @@
 </div>
 <!-- Features Profile Screenshots End -->
 
+<!-- Features Project Start -->
+<div id="">
+	<!-- Container Start -->
+	<div class="container-fluid">
+		<!-- Row Start -->
+		<section class="row-fluid">
+			<div class="span12">
+				<div class="featuresProject">
+					<div class="left">
+						<h2>
+							<span>
+								<img src="/images/features/projects-mini.png" alt="Projects" />
+							</span>
+							Projects
+						</h2>
+						<p>Choose Type. Enter Details. We Notify. Choose Winner.</p>
+						<p>Music Gateway is like no other music service. Yes we are a business platform, but we are not like LinkedIn. We support your career, but we don't sell, distribute or promote your music directly to the public like so many other sites.</p>
+						<p>Allow us to explain... Millions of people use LinkedIn, in fact +200 million worldwide. LinkedIn does have benefits, networking groups & connecting to new clients & keeping that work CV up to scratch.</p>
+						<p>As far as the Music Industry is concerned, LinkedIn connections can be fairly random, then nothing tends to happen. Sound familiar? The main reason is that connections aren't linked to a purpose, an end goal or an objective! LinkedIn & many other sites don't provide work flow functionality & that all important end result.</p>
+						<p>So, what's different about Music Gateway? In one word "Projects".</p>
+						<p><a href="/projects" title="View Project Types" class="blueButton">View Project Types</a></p>
+					</div>
+					<div class="right">
+						<img src="/images/features/four-projects.png" alt="Projects" />
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Row End -->
+	</div>
+	<!-- Contaciner End -->
+</div>
+<!-- Features Project End -->
+
+<!-- Sign Up Banner Start -->
+<div class="signUpBanner">
+	<!-- Container Start -->
+	<div class="container-fluid">
+		<!-- Row Start -->
+		<section class="row-fluid">
+			<div class="span12">
+				<div class="signUp-fullWidth">
+					<div class="left">
+						<h3>Show The World Your Musical Talent Today!</h3>
+						<p>You never know what talent you could find...</p>
+					</div>
+					<div class="right">
+						<a href="/sign-up" title="Sign Up &amp; Create My Project" class="greenButton">Sign Up &amp; Create My Project</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Row End -->
+	</div>
+	<!-- Contaciner End -->
+</div>
+<!-- Sign Up Banner End -->
+
+<!-- Features Workspace Start -->
+<div id="">
+	<!-- Container Start -->
+	<div class="container-fluid">
+		<!-- Row Start -->
+		<section class="row-fluid">
+			<div class="span12">
+				<div class="features-profileInner workSpaceArea">
+					<h2>
+						<span>
+							<img src="/images/features/workspace-mini.png" alt="Workspace" />
+						</span>
+						Workspace
+					</h2>
+					<p>Our workspaces are unique areas assigned to your Projects. Once your project starts the workspace area allows you to communicate, manage your audio &amp; all types of project files such as midi.</p>
+					<p>Rather than using any third party sites such as Dropbox or Wetransfer, which have their limitation, our workspaces provide multiple access to your project workers, where all members can upload, download &amp; submit files for feedback.</p>
+					<p>Workspace areas are secure, offering protection by a 2048 bit SSL Certificate. Our communication features include timeline feedback on audio files, plus project member messaging &amp; comments.</p>
+					<div class="largeScreen">
+						<ul>
+							<li>
+								<img src="/images/features/screen.jpg" alt="Workspace" />
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Row End -->
+	</div>
+	<!-- Contaciner End -->
+</div>
+<!-- Features Workspace End -->
+
+<!-- Features Account Start -->
+<div class="featuresAccount">
+	<!-- Container Start -->
+	<div class="container-fluid">
+		<!-- Row Start -->
+		<section class="row-fluid">
+			<div class="span12">
+				<div class="features-profileInner accountArea">
+					<h2>
+						<span>
+							<img src="/images/features/account-mini.png" alt="Account" />
+						</span>
+						Account
+					</h2>
+					<h3>Secure. Transact. Manage.</h3>
+					<p>Once registered, you create a transaction account. Don't worry, we DO NOT take any form of payment details or ask for any money, you simply choose which currency you wish to link to your account. This can be one of three different currencies, US Dollars, UK Pounds Sterling &amp; Euros.</p>
+					<p>Your account allows you to receive payment from other users for your project work, or to pay other users for their services. Pitching for project work is 100% free, so is your account, we charge a percentage fee based on fixed fee project work you receive.</p>
+					<p>Your account is protected by an encrypted SSL certificate. Monies received from other currencies to your account are automatically converted into your own currency. Crediting & withdrawing funds is fast and easy with various payment options available.</p>
+				</div>
+			</div>
+		</section>
+		<!-- Row End -->
+	</div>
+	<!-- Contaciner End -->
+</div>
+<!-- Features Account End -->
+
+
+
+
+
+
 
 <?php
 	include('../includes/footer.php');
