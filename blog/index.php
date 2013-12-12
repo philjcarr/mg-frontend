a12280eb5c2182b357fbc6337df5e2c197c4529f
philcarr88 <phil.carr@musicgateway.net>
2013-12-12 22:39:07 +0000

    Started Blog Layout

---
+++ blog/index.php
@@ -0,0 +1,135 @@
+<?php
+	include('../includes/header.php');
+?>
+
+<!-- Container Start -->
+<div class="container-fluid">
+	<section class="row-fluid">
+		<div class="span12">
+			<h1 class="topHeading">Blog</h1>
+		</div>
+	</section>
+</div>
+<!-- Contaciner End -->
+
+<!-- Container Start -->
+<div class="container-fluid">
+	<section class="row-fluid">
+		<div class="span12">
+			<div class="topDivide"><!-- Top Divide --></div>
+		</div>
+	</section>
+</div>
+<!-- Contaciner End -->
+
+<!-- Container Start -->
+<div class="container-fluid">
+	<!-- Row Start -->
+	<section class="row-fluid">
+		<!-- Span 8 Start -->
+		<div class="span8">
+			<div class="leftCol">
+				
+				<article class="first">
+					<h2>Heading</h2>
+					<p class="strip">
+						<span class="tab first date">14 January 2014</span>
+						<span class="tab author">By <span>Phil Carr</span></span>
+						<span class="tab comment"><span>10</span> Comments</span>
+					</p>
+					<p>blog intro text...</p>
+				</article>
+
+				<article>
+					<h2>Heading</h2>
+					<p class="strip">
+						<span class="tab first date">14 January 2014</span>
+						<span class="tab author">By <span>Phil Carr</span></span>
+						<span class="tab comment"><span>10</span> Comments</span>
+					</p>
+					<p>blog intro text...</p>
+				</article>
+
+				<article>
+					<h2>Heading</h2>
+					<p class="strip">
+						<span class="tab first date">14 January 2014</span>
+						<span class="tab author">By <span>Phil Carr</span></span>
+						<span class="tab comment"><span>10</span> Comments</span>
+					</p>
+					<p>blog intro text...</p>
+				</article>
+				
+			</div>
+		</div>
+		<!-- Span 8 End -->
+		<!-- Span 4 Start -->
+		<div class="span4">
+			<div class="rightCol">
+				<article class="first">
+					<h3>Sign up as a Artist Manager</h3>
+					<p>It’s totally <strong>FREE!</strong></p>
+					<a href="/sign-up" title="Sign Up" class="greenButton">Sign Up</a>
+				</article>
+				<article>
+					<ul>
+						<li class="first artists">
+							<a href="/how-it-works/artists" title="Artists" class="">Artists</a>
+						</li>
+						<li class="artistsManagement active">
+							<a href="/how-it-works/artist-management" title="Artist Management" class="">Artist Management</a>
+						</li>
+						<li class="bands">
+							<a href="/how-it-works/bands" title="Bands" class="">Bands</a>
+						</li>
+						<li class="mastering">
+							<a href="/how-it-works/mastering" title="Mastering" class="">Mastering</a>
+						</li>
+						<li class="musicians">
+							<a href="/how-it-works/musicians" title="Musicians" class="">Musicians</a>
+						</li>
+						<li class="producers">
+							<a href="/how-it-works/producers-and-remixers" title="Producers">Producers &amp; Remixers</a>
+						</li>
+						<li class="publishers">
+							<a href="/how-it-works/publishers" title="Publishers" class="">Publishers</a>
+						</li>
+						<li class="recordLabels">
+							<a href="/how-it-works/record-labels" title="Record Labels" class="">Record Labels</a>
+						</li>
+						<li class="songwriters">
+							<a href="/how-it-works/songwriters" title="Songwriters" class="">Songwriters</a>
+						</li>
+						<li class="studios">
+							<a href="/how-it-works/studios" title="Studios" class="">Studios</a>
+						</li>
+						<li class="vocalists">
+							<a href="/how-it-works/vocalists" title="Vocalists" class="">Vocalists</a>
+						</li>
+						<li class="industryServices">
+							<a href="/how-it-works/industry-services" title="Industry Services" class="">Industry Services</a>
+						</li>
+					</ul>
+				</article>
+				<article class="testimonial">
+					<p class="comment">The alarming thing is, the mistakes that produce these regrets are all errors of omission. You forget your dreams, ignore your family, suppress your feelings, neglect your friends, and forget to be happy.”</p>
+					<p class="author">Andrew Davies</p>
+					<p class="company">
+						<a href="/partners" title="">Music Week</a>,&nbsp;
+						<span>UK</span>
+					<p>
+				</article>
+			</div>
+		</div>
+		<!-- Span 4 End -->
+	</section>
+	<!-- Row End -->
+</div>
+<!-- Contaciner End -->
+
+
+<?php
+	include('../includes/footer.php');
+?>
+
+
